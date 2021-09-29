b1ab9f5d4932c3ca716d9fee4f48b5b6f9b90266
Joel Velis <joelvelis94@gmail.com>
2021-09-29 07:18:47 -0400

    created bet table

--- db/migrate/20210929111312_create_bets.rb
+++ db/migrate/20210929111312_create_bets.rb
@@ -3,6 +3,13 @@ class CreateBets < ActiveRecord::Migration[6.1]
     create_table :bets do |t|
       t.integer :bet_maker
       t.integer :bet_receiver
+      t.string :name
+      t.string :category
+      t.datetime :start_date, default: -> { 'CURRENT_TIMESTAMP' }
+      t.date :end_date
+      t.float :wager_amount
+      t.string :status, default: "open"
+      t.text :rules
 
       t.timestamps
     end
