988f1515d5f9139a568226bc860c437c3e10329e
Joel Velis <joelvelis94@gmail.com>
2021-10-08 10:18:01 -0400

    succesfully setup notifications specific to each user

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,7 +1,9 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from "NotificationsChannel"
+    # stream_from "NotificationsChannel"
+    stream_from "notifications_#{current_user.id}"
+
   end
 
   def unsubscribed
